90b18e87881a4afe0c92f59e10e5d40738d2e51a
marcescala <marcescala@yahoo.com>
2024-04-04 16:48:03 -0500

    primeros pasos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter a number:");
+            string userInput = Console.ReadLine();
+
+            // Convertir la entrada del usuario a un número entero
+            if (int.TryParse(userInput, out int number))
+            {
+                Console.WriteLine($"You entered: {number}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+
+        }
+
+    }
+
+}
--- obj/Debug/net8.0/CSP001-guess-the-number.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSP001-guess-the-number.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSP001-guess-the-number")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e1720483d03b03a74b4ff81b9795d2162ba190")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faeacf43e66285902530c3270e476a8854090ee8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSP001-guess-the-number")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSP001-guess-the-number")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
